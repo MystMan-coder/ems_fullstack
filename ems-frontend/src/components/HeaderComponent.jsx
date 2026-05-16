a0edca172ff1c6ef5d3b04105065794e1f78a77d
Dhananjay Kumar <officialdkr007@gmail.com>
2026-05-16 12:36:16 +0530

    Connected to bacnkend and added Headers

---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+                <a class="navbar-brand" href="https://dhananjaykumar.vercel.app/">Employees Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
